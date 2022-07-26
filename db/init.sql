4874fd9a0bdef80682e4d5e309311d1f9fe0b85a
Sicorschi <sicorschiigor1991@gmail.com>
2022-07-26 23:47:58 +0200

    updated the env variables

--- db/init.sql
+++ db/init.sql
@@ -20,5 +20,5 @@ VALUES(
         23
 );
 
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password'; 
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY ''; 
 flush privileges;
--- web/index.js
+++ web/index.js
@@ -7,7 +7,7 @@ const connection = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "password",
+  password: process.env.MYSQL_PASSWORD || "",
   database: process.env.MYSQL_DATABASE || "test",
 });
 
